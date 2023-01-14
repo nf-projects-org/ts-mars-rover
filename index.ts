380a5b78ad7be84476fdc13a78d488c298db1af6
Nigel Faria <nigel.faria@gmail.com>
2023-01-14 22:25:35 +0000

    Formatting

--- index.ts
+++ index.ts
@@ -1,16 +1,16 @@
-import { InputParser, FileInputParser, BrowserInputParser  } from "./src/controller/InputParser";
+import { InputParser, FileInputParser, BrowserInputParser } from "./src/controller/InputParser";
 import { MarsStateController } from "./src/controller/MarsStateController";
 
-let inputParser:InputParser;
+let inputParser: InputParser;
 
 if (typeof window !== 'undefined') {
-    inputParser = new BrowserInputParser();
-  } else {
-   inputParser = new FileInputParser(); 
+  inputParser = new BrowserInputParser();
+} else {
+  inputParser = new FileInputParser();
 }
 
 
-export function start(){
+export function start() {
   let marsStateController = new MarsStateController(inputParser);
   marsStateController.start();
 }
