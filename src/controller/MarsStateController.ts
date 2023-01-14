bc3248b60b68dc0386bfbe750156980e1ac9b63f
Nigel Faria <nigel.faria@gmail.com>
2023-01-14 22:26:59 +0000

    executeRover command functionality

--- src/controller/MarsStateController.ts
+++ src/controller/MarsStateController.ts
@@ -10,47 +10,54 @@ export class MarsStateController {
     static MOVETURN = Command.commandTypes[2];
     static UNKNOWN = Command.commandTypes[3];
 
-    private inputParser:InputParser;
-    private planetMap:PlanetMap;
-    private vehicles:Vehicle[];
+    private inputParser: InputParser;
+    private planetMap: PlanetMap;
+    private vehicles: Vehicle[];
 
-    constructor(inputParser:InputParser){
+    constructor(inputParser: InputParser) {
         this.inputParser = inputParser;
         this.vehicles = [];
     }
 
-    start(){
+    /**
+     * Main execution function
+     */
+    start() {
         let commands = this.inputParser.getCommands();
-        console.log(commands);
-        if(commands.length > 0){
+        if (commands.length > 0) {
             this.executeCommand(Command.getCreateMapCommands(commands)[0]);
-            Command.getCreateRoverCommands(commands).forEach(this.executeCommand);
+            let createRoverCommands = Command.getCreateRoverCommands(commands);
+            createRoverCommands.forEach(this.executeCommand);
+            let moveTurnRoverCommands = Command.getMoveTurnRoverCommands(commands);
+            moveTurnRoverCommands.forEach((command, index) => this.executeRoverCommand(command, index));
         }
         console.log(this.getState());
-
     }
 
-    getState(){
-        let output = `${this.planetMap}\n${this.vehicles}`
-        return output;
+    getState() {
+        return `${this.planetMap}\n${this.vehicles}`;
     }
 
-    private executeCommand = (command:Command) => {
-        if (command.getCommandType() == MarsStateController.CREATEMAP){
-            let [xString,yString] = command.getValues();
-            this.planetMap = RectangularMap.createMapFromString(xString,yString);
-        } else if(command.getCommandType() == MarsStateController.CREATEROVER){
+    private executeCommand = (command: Command) => {
+        if (command.getCommandType() == MarsStateController.CREATEMAP) {
+            let [xString, yString] = command.getValues();
+            this.planetMap = RectangularMap.createMapFromString(xString, yString);
+        } else if (command.getCommandType() == MarsStateController.CREATEROVER) {
             let [xString, yString, compassString] = command.getValues();
-            let roverPostion = Position.createPosition([xString,yString])
+            let roverPostion = Position.createPosition([xString, yString]);
             let compassHeading = CompassHeading.createHeading(compassString);
-            let vehiclePositions:Position[] = [];
-            let roverInstruments:Instrument[] = [];
-            this.vehicles.push(MarsRover.createRover(this.planetMap, roverPostion, compassHeading, vehiclePositions,roverInstruments ));
+            let vehiclePositions: Position[] = [];
+            let roverInstruments: Instrument[] = [];
+            this.vehicles.push(MarsRover.createRover(this.planetMap, roverPostion, compassHeading, vehiclePositions, roverInstruments));
         }
     }
 
+    private executeRoverCommand = (command: Command, index: number) => {
+        if (command.getCommandType() == MarsStateController.MOVETURN) {
+            if (index >= 0) {
+                this.vehicles[index].moveOrTurn(command);
+            }
+        }
 
-    
-
-
+    }
 }
